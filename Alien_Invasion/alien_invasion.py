cddcdadbf57a6084e1df8ca691d1c0353b8c1bf6
riyyan <riyyanhassan789@gmail.com>
2025-11-28 00:32:11 +0500

    Learned how to install pygame module using pip, also learned how to create a game window (surface) using pygame and how to run the window and listen for exiting event.

---
+++ Alien_Invasion/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+class Alien_Invasion:
+    '''a class to represent game elements'''
+
+    def __init__(self):
+        '''constructor to initialize attributes and pygame'''
+        pygame.init()
+        self.setting = Settings()
+        #create a game window
+        self.screen = pygame.display.set_mode((self.setting.width,
+                                               self.setting.height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        '''method to keep the game running and update changes'''
+        while True:
+            # listen for events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #display the most recent screen
+            pygame.display.flip()
+            self.screen.fill(self.setting.bg_color) # fill screen with bg color
+
+if __name__ == '__main__':
+    ai = Alien_Invasion()
+    ai.run_game()
---
+++ Alien_Invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    '''a class to represent settings of Alien invasion'''
+
+    def __init__(self):
+        # initializes the settings for game
+        self.width = 800
+        self.height = 800
+        self.bg_color = (120,120,120)
