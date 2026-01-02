b0518a73d8dab52c5b5df5935dc3f7f5f53ed302
riyyan <riyyanhassan789@gmail.com>
2026-01-02 14:11:35 +0500

    Learned how to plot lines on graphs using number stored in a list and also learned how to color and label the graphs.

--- Data_Visualization/scatter_square.py
+++ Data_Visualization/scatter_square.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-# squares = [number*number for number in range(6)]
-input_values = [1,2,3,4,5]
-squares = [1,4,9,16,25]
-# print(plt.style.available)
+x_values = range(1,1001)
+y_values = [number**2 for number in x_values]
+
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.plot(input_values,squares,linewidth = 3)
+ax.scatter(x_values,y_values,s=10,c=y_values,cmap=plt.cm.Reds)
+ax.axis([0,1100,0,1100000])
 ax.set_title('Square Numbers',fontsize=24)
 ax.set_xlabel('Value',fontsize=14)
 ax.set_ylabel('Square of Values',fontsize=14)
-ax.tick_params('both',labelsize=14)
+ax.tick_params('both',which = 'major', labelsize=14)
 plt.show()
