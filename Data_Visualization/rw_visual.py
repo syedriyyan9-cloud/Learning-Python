3b5d5446680991b3b72ef0cc4ce53e6290484a05
riyyan <riyyanhassan789@gmail.com>
2026-01-06 18:21:57 +0500

    Learned how to draw starting and ending points on a graph, and also learned how to set axis visibility.

--- Data_Visualization/rw_visual.py
+++ Data_Visualization/rw_visual.py
@@ -3,10 +3,17 @@ from random_walk import RandomWalk
 import matplotlib.pyplot as plt
 while True:
     plt.style.use('dark_background')
-    rw = RandomWalk()
+    # TIY 15-3
+    rw = RandomWalk(5_000)
     rw.fill_walk()
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values,c=rw.y_values,s=15,cmap=plt.cm.Reds)
+    fig, ax = plt.subplots(figsize=(10,6))
+    ax.plot(rw.x_values,rw.y_values,linewidth = 5)
+    points = range(rw.n_points)
+    # ax.scatter(rw.x_values, rw.y_values,c=points,s=1,cmap=plt.cm.Reds,edgecolors="none")
+    # ax.scatter(rw.x_values[0],rw.y_values[0],c='green',s=5)
+    # ax.scatter(rw.x_values[-1],rw.y_values[-1],c='blue',s=5)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
     plt.show()
     keep_running = input("press 'n' to exit")
     if keep_running == 'n':
