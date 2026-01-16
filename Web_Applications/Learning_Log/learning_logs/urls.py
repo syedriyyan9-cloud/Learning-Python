b79073c2cbfbacc4960e270c906571f89945218f
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-01-16 19:47:07 +0500

    Learned how to get id from browser and how to then use that id to run queries.

--- Web_Applications/Learning_Log/learning_logs/urls.py
+++ Web_Applications/Learning_Log/learning_logs/urls.py
@@ -8,5 +8,7 @@ app_name = 'learning_logs'
 
 urlpatterns = [
     # homepage
-    path('',views.index, name = 'index')
+    path('',views.index, name = 'index'),
+    path('topics/',views.topics, name = 'topics'),
+    path('topics/<int:topic_id>/',views.topic, name = 'topic'),
 ]
--- Web_Applications/Learning_Log/learning_logs/views.py
+++ Web_Applications/Learning_Log/learning_logs/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from .models import Topics
+
 # Create your views here.
 def index(request):
     """renders Home page"""
     return render(request,'learning_logs/index.html')
+
+def topics(request):
+    """Displays topics to topic.html template"""
+    topics = Topics.objects.order_by('date')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """show a single topic and all its entries"""
+    topic = Topics.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
