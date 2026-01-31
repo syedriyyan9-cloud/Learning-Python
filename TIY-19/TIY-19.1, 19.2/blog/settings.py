9befb34085bdd3ea4b5133832dbacf06b4050f9e
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-01-31 13:09:27 +0500

    Complete TIY 19.2

--- TIY-19/TIY-19.1, 19.2/blog/settings.py
+++ TIY-19/TIY-19.1, 19.2/blog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # custom apps
     'blogs',
+    'users',
     # default apps
     'django.contrib.admin',
     'django.contrib.auth',
--- TIY-19/TIY-19.1, 19.2/blog/urls.py
+++ TIY-19/TIY-19.1, 19.2/blog/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
+    path('users/', include('users.urls')),
 ]
