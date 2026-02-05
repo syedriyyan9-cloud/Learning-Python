d101abdc5d190d5cd2986eeef214b13335aeef10
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-02-05 19:38:41 +0500

    Added functionality to allow users to be associated with topics they create, and also to not allow users to see each other topic entires.

--- Web_Applications/Learning_Log/learning_logs/models.py
+++ Web_Applications/Learning_Log/learning_logs/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Topics(models.Model):
     """A topic the user is learning about."""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length = 200)
     date = models.DateTimeField(auto_now_add = True)
 
--- Web_Applications/Learning_Log/learning_logs/views.py
+++ Web_Applications/Learning_Log/learning_logs/views.py
@@ -6,6 +6,8 @@ from .forms import TopicForm, EntryForm
 
 from django.contrib.auth.decorators import login_required
 
+from django.http import Http404
+
 # Create your views here.
 def index(request):
     """renders Home page"""
@@ -14,7 +16,7 @@ def index(request):
 @login_required
 def topics(request):
     """Displays topics to topic.html template"""
-    topics = Topics.objects.order_by('date')
+    topics = Topics.objects.filter(owner = request.user).order_by('date')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +24,8 @@ def topics(request):
 def topic(request, topic_id):
     """show a single topic and all its entries"""
     topic = Topics.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -34,7 +38,9 @@ def new_topic(request):
     else:   # data submitted, process data
         form = TopicForm(data = request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # display a blank or invalid form
     context = {'form':form}
@@ -61,6 +67,8 @@ def edit_entry(request, entry_id):
     """Edit existing entires"""
     entry = Entry.objects.get(id = entry_id)
     topic = entry.topic
+    if topic.owner != request.owner:
+        raise Http404
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
